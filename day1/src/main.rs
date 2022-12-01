58994e16bc5d0039f9202d16916a31411b5a6f17
Joey de Ruiter <contact@joeyderuiter.me>
2022-12-01 13:34:52 +0100

    Added a shorter version for part2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -39,8 +39,22 @@ fn part2(input: &str) -> usize {
     calories.iter().take(3).sum()
 }
 
+fn part2_shorter(input: &str) -> usize {
+    let mut values: Vec<usize> = input.split("\n\n").map(|x| 
+        x.split("\n")
+            .map(|y| y.parse::<usize>().unwrap_or_default())
+            .sum()
+    ).collect();
+
+    values.sort_by(|a, b| b.cmp(a));
+
+    values.iter().take(3).sum()
+}
+
 fn main() {
     let input = include_str!("input.txt");
+
     println!("part1: {}", part1(input));
     println!("part2: {}", part2(input));
+    println!("part2: {}", part2_shorter(input));
 }
