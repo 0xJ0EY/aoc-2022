8afd6d144b2ab9244b4bf1727952e5671b977b83
Joey de Ruiter <contact@joeyderuiter.me>
2022-12-04 13:25:11 +0100

    Day 4 completed

---
+++ day4/src/main.rs
@@ -0,0 +1,66 @@
+use std::{str::FromStr, num::ParseIntError, cmp};
+
+struct Section {
+    pub start: usize,
+    pub end: usize
+}
+
+impl FromStr for Section {
+    type Err = ParseIntError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (start, end) = s.split_once('-').unwrap();
+
+        let start_fromstr   = start.parse::<usize>()?;
+        let end_fromstr     = end.parse::<usize>()?;
+
+        Ok(Section { start: start_fromstr, end: end_fromstr })
+    }
+}
+
+impl Section {
+    fn contains(&self, other: &Section) -> bool {
+        let start   = self.start <= other.start && self.end >= other.end;
+        let end     = self.start >= other.start && self.end <= other.end;
+
+        start || end
+    }
+
+    fn overlap(&self, other: &Section) -> bool {
+        let start   = cmp::max(self.start, other.start) as i64;
+        let end     = cmp::min(self.end, other.end) as i64;
+
+        end - start >= 0
+    }
+}
+
+fn part1(input: &str) -> usize {
+    return input
+        .split_terminator('\n')
+        .map(|line| {
+            let (start, end) = line.split_once(',').unwrap();
+
+            let lhs = Section::from_str(start).unwrap();
+            let rhs = Section::from_str(end).unwrap();
+
+            if lhs.contains(&rhs) { 1 } else { 0 }
+        }).sum();
+}
+
+fn part2(input: &str) -> usize {
+    return input.split_terminator('\n')
+        .map(|line| {
+            let (start, end) = line.split_once(',').unwrap();
+
+            let lhs = Section::from_str(start).unwrap();
+            let rhs = Section::from_str(end).unwrap();
+
+            if lhs.overlap(&rhs) { 1 } else { 0 }
+        }).sum();
+}
+
+fn main() {
+    let input = include_str!("input.txt");
+    println!("part1: {}", part1(input));
+    println!("part2: {}", part2(input));
+}
